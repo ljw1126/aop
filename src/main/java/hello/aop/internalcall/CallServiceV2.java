0cb96109437422f73b208263092bdc905fc1e403
leejinwoo <leejinwoo1126@gmail.com>
2024-01-05 15:46:27 +0900

    feat: 프록시와 내부 호출 대안2 - 지연 조회

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,30 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+
+    //private final ApplicationContext applicationContext;
+    private final ObjectProvider<CallServiceV2> callServiceV2ObjectProvider;
+
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceV2ObjectProvider) {
+        this.callServiceV2ObjectProvider = callServiceV2ObjectProvider;
+    }
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV2 = callServiceV2ObjectProvider.getObject();
+        callServiceV2.internal(); // 지연 전략
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
